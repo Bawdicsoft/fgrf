4b9cd4fcad16b5946c189197a7169c6eba1d8298
Ashraf Ali <ali4282271@gmail.com>
2023-12-28 19:13:20 +0500

    donation appeal palestineEmergency

---
+++ app/donation-appeal/page.tsx
@@ -0,0 +1,13 @@
+import GhazaUnderTakDocs from "@/components/donationApeal/palestineEmergency/gazaUnderTack";
+import PalestinBannerSection from "@/components/donationApeal/palestineEmergency/palestinBannerSection";
+
+export default function donationAppeal() {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <PalestinBannerSection />
+            <GhazaUnderTakDocs />
+        </div>
+
+
+    );
+}
