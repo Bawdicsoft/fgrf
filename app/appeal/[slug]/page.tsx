cac5087ead7d3cb8018b1e2cd83a53baea663e78
Ashraf Ali <ali4282271@gmail.com>
2023-12-29 14:58:29 +0500

    create dynamic page appeal

---
+++ app/appeal/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import DonateSection from "@/components/donationApeal/feedOurWold/donatSection";
+import FeedOurWoldBannerSection from "@/components/donationApeal/feedOurWold/feedOurWoldBannerSection";
+import GhazaUnderTakDocs from "@/components/donationApeal/palestineEmergency/gazaUnderTack";
+import PalestinBannerSection from "@/components/donationApeal/palestineEmergency/palestinBannerSection";
+
+export default function donationAppeal({ params }: { params: { slug: string } }) {
+    const myParams = params.slug;
+    return (
+        <div className="max-w-7xl mx-auto">
+            {/* Palestine Emergency */}
+            {myParams === "palestine-emergency" && <div>
+                <PalestinBannerSection />
+                <GhazaUnderTakDocs />
+            </div>}
+            {/* feed-our-world */}
+            {myParams === "feed-our-world" && <div>
+                <FeedOurWoldBannerSection />
+                <DonateSection />
+            </div>}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    );
+}
