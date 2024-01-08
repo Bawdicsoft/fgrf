d0e12137243cdd484d0a1edb5c2416309fe0acd5
Ashraf Ali <ali4282271@gmail.com>
2024-01-08 19:46:47 +0500

    update import

--- components/newUi/home/News.tsx
+++ components/newUi/home/News.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import NewsSlider from './NewsSlider';
-import ContactUs from './contactus';
+import ContactUs from './Contactus';
 
 
 const NewsSection: React.FC = () => {
