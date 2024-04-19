99d0eb0b3981639cbc62cbec2f3fe349451bdbf3
ashrafali2000 <ali4282271@gmail.com>
2024-04-19 14:51:51 +0500

    update one images domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,26 +11,26 @@ const nextConfig = {
   // images: {
   //   domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
   // },
-  // images: {
-  //   remotePatterns: [
-  // {
-  //   protocol: "https",
-  //   hostname: "firebasestorage.googleapis.com",
-  //   port: "",
-  //   pathname: "/v0/b/fgrf-dc455.appspot.com/**",
-  // },
-  // {
-  //   protocol: "https",
-  //   hostname: "lh3.googleusercontent.com",
-  //   port: "",
-  //   pathname: "/**",
-  // },
-  //   ],
-  // },
   images: {
-    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
-    formats: ["image/webp", "image/jpeg", "image/png", "image/gif"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/fgrf-dc455.appspot.com/**",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "lh3.googleusercontent.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+    ],
   },
+  // images: {
+  //   domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
+  //   formats: ["image/webp", "image/jpeg", "image/png", "image/gif"],
+  // },
 };
 
 module.exports = nextConfig;
