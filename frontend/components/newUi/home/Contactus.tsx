d588ab8bedcee481060b64b87b822deaa9c59203
ashrafali2000 <ali4282271@gmail.com>
2024-05-18 18:12:03 +0500

    add contactus message to email

--- frontend/components/newUi/home/Contactus.tsx
+++ frontend/components/newUi/home/Contactus.tsx
@@ -1,15 +1,39 @@
 import Link from "next/link";
 import { motion, useAnimation } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import AnimationTop from "./AnimationTop";
 import AnimationBottom from "./AnimationBtm";
+import axios from "axios";
 const ContactUs: React.FC = () => {
+  const [formData, setFormData] = useState<any>({});
+  const handleChange = (e: any) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    axios
+      .post("/api/contactus", {
+        email: formData.email,
+        name: formData.userName,
+        phoneNo: formData.phoneNo,
+        userMessage: formData.userMessage,
+      })
+      .then(function (response) {
+        console.log("user response", response);
+      })
+      .catch(function (error) {
+        console.log("error--->", error);
+      });
+  };
+
   return (
     <section className="py-10 ">
-      <div className="flex flex-col justify-center items-center px-5 bg-white rounded-lg">
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col justify-center items-center px-5 bg-white rounded-lg"
+      >
         <AnimationTop>
           <motion.div
-            // className="box"
             whileHover={{ scale: 1.2, filter: "brightness(1.2)" }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
@@ -29,13 +53,14 @@ const ContactUs: React.FC = () => {
                 </AnimationTop>
                 <AnimationBottom>
                   <motion.div
-                    // className="box"
                     whileHover={{ scale: 1.2 }}
                     transition={{ type: "spring", stiffness: 400, damping: 10 }}
                   >
                     <div className="shadow-lg  rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-2">
                       <input
                         type="text"
+                        id="userName"
+                        onChange={handleChange}
                         className="bg-[#19afaf] outline-none text-white font-extrabold p-0.5 text-2xl w-full"
                       />
                     </div>
@@ -51,13 +76,14 @@ const ContactUs: React.FC = () => {
                 </AnimationTop>
                 <AnimationBottom>
                   <motion.div
-                    // className="box"
                     whileHover={{ scale: 1.2 }}
                     transition={{ type: "spring", stiffness: 400, damping: 10 }}
                   >
                     <div className="shadow-lg  rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-2">
                       <input
                         type="text"
+                        id="phoneNo"
+                        onChange={handleChange}
                         className="bg-[#19afaf] text-white outline-none font-extrabold p-0.5 text-2xl w-full"
                       />
                     </div>
@@ -72,40 +98,20 @@ const ContactUs: React.FC = () => {
                 </AnimationTop>
                 <AnimationBottom>
                   <motion.div
-                    // className="box"
                     whileHover={{ scale: 1.2 }}
                     transition={{ type: "spring", stiffness: 400, damping: 10 }}
                   >
                     <div className="shadow-lg  rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-2">
                       <input
                         type="text"
+                        id="email"
+                        onChange={handleChange}
                         className="bg-[#19afaf] outline-none text-white font-extrabold p-0.5 text-2xl w-full"
                       />
                     </div>
                   </motion.div>
                 </AnimationBottom>
               </div>
-              {/* <div className="">
-                <AnimationTop>
-                  <label className="font-bold text-sky-800 pl-2">
-                    Full Name
-                  </label>
-                </AnimationTop>
-                <AnimationBottom>
-                  <motion.div
-                    // className="box"
-                    whileHover={{ scale: 1.2 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                  >
-                    <div className="shadow-lg  rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-3">
-                      <input
-                        type="text"
-                        className="bg-[#19afaf] text-white font-extrabold p-0.5 text-2xl max-w-[190px]"
-                      />
-                    </div>
-                  </motion.div>
-                </AnimationBottom>
-              </div> */}
             </div>
           </div>
           <div>
@@ -115,13 +121,14 @@ const ContactUs: React.FC = () => {
               </AnimationTop>
               <AnimationBottom>
                 <motion.div
-                  // className="box"
                   whileHover={{ scale: 1.2 }}
                   transition={{ type: "spring", stiffness: 400, damping: 10 }}
                 >
                   <div className="shadow-lg  rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0">
                     <textarea
                       rows={4}
+                      id="userMessage"
+                      onChange={handleChange}
                       className="bg-[#19afaf] outline-none text-white font-extrabold p-0.5 text-2xl resize-none w-full"
                     ></textarea>
                   </div>
@@ -132,18 +139,20 @@ const ContactUs: React.FC = () => {
         </div>
         <AnimationTop>
           <motion.div
-            // className="box"
             whileHover={{ scale: 1.2, filter: "brightness(1.2)" }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            <div className="rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-3">
+            <button
+              type="submit"
+              className="rounded-md bg-gradient-to-t from-sky-800 via-green-200 to-sky-800 px-4 py-0 my-3"
+            >
               <span className="bg-white px-2 text-sky-900 font-extrabold p-0.5 text-2xl">
                 Send
               </span>
-            </div>
+            </button>
           </motion.div>
         </AnimationTop>
-      </div>
+      </form>
     </section>
   );
 };
