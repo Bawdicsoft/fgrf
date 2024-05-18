8c6bdf5111be02e2df164190c5d895d7b2760ee2
ashrafali2000 <ali4282271@gmail.com>
2024-05-18 18:56:49 +0500

    add nodmailer into backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const checkOut = require("./routes/checkOut");
+const contactUs = require("./routes/contactus");
 const app = express();
 app.use(cors());
 const port = 8000;
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // checkOut MiddleWare
 app.use("/stripe", checkOut);
+app.use("/contactus", contactUs);
 app.get("/", function (req, res) {
   res.send("Server is running");
 });
